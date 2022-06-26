4925676562fb5fe2a370608b5275ba063224b665
vili92 <vileonst92@gmail.com>
2022-06-26 16:51:29 +0500

    Product

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.CharField(max_length=1000, blank=True, null=True)
+    amount = models.IntegerField(default=1)
