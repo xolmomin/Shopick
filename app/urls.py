cdfb7a20e7a7fb3cbe7753d512e66c23fd8db730
online_shop_chala <zabborovmuhammad291@gmail.com>
2022-06-27 20:54:29 +0500

    all temlates done

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path
 
-from views import  AboutPage
+from views import AboutPage, IndexPage, BlogPage, CartPage, CheckoutPage, ContactPage, LoginPage, MyAccPage, ShopPage, \
+    ShopListPage, SingleBlogPage, SingleProductPage, ThankYouPage, WishListPage
 
 urlpatterns = [
-    path('about/',AboutPage.as_view(),name='about'),
+    path('', IndexPage.as_view(), name='index'),
+    path('blog/', BlogPage.as_view(), name='blog'),
+    path('cart/', CartPage.as_view(), name='cart'),
+    path('checkout/', CheckoutPage.as_view(), name='checkout'),
+    path('contact/', ContactPage.as_view(), name='contact'),
+    path('about/', AboutPage.as_view(), name='about'),
+    path('login/', LoginPage.as_view(), name='login'),
+    path('my-account/', MyAccPage.as_view(), name='my_acc'),
+    path('shop/', ShopPage.as_view(), name='shop'),
+    path('shop-list/', ShopListPage.as_view(), name='shop-list'),
+    path('single-blog/', SingleBlogPage.as_view(), name='single_blog'),
+    path('single-product/', SingleProductPage.as_view(), name='single_product'),
+    path('thank-you/', ThankYouPage.as_view(), name='thank_you'),
+    path('wish-list/', WishListPage.as_view(), name='wishlist'),
+
 ]
+
--- app/views.py
+++ app/views.py
@@ -2,7 +2,57 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
+class IndexPage(TemplateView):
+    template_name = 'app/index.html'
+
+
+class BlogPage(TemplateView):
+    template_name = 'app/blog.html'
+
+
+class CartPage(TemplateView):
+    template_name = 'app/cart.html'
+
+
+class CheckoutPage(TemplateView):
+    template_name = 'app/checkout.html'
+
+
+class ContactPage(TemplateView):
+    template_name = 'app/contact.html'
 
 
 class AboutPage(TemplateView):
     template_name = 'app/about.html'
+
+
+class LoginPage(TemplateView):
+    template_name = 'app/login.html'
+
+
+class MyAccPage(TemplateView):
+    template_name = 'app/my-account.html'
+
+
+class ShopPage(TemplateView):
+    template_name = 'app/shop.html'
+
+
+class ShopListPage(TemplateView):
+    template_name = 'app/shop-list.html'
+
+
+class SingleBlogPage(TemplateView):
+    template_name = 'app/single-blog.html'
+
+
+class SingleProductPage(TemplateView):
+    template_name = 'app/single-product.html'
+
+
+class ThankYouPage(TemplateView):
+    template_name = 'app/thank-you.html'
+
+
+class WishListPage(TemplateView):
+    template_name = 'app/wishlist.html'
